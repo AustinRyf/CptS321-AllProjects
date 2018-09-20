8bbd4480cfc59a14587a6d75b680ec458d5a4670
aryf <aryf@eecs.wsu.edu>
2018-09-19 22:01:28 -0700

    Project completed

---
+++ NotepadApp/NotepadApp HW/NotepadApp Homework/NotepadApp Homework/Fibonacci.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace NotepadApp_Homework 
+{
+    class FibonacciTextReader : System.IO.TextReader
+    {
+        int totalLines = 0;
+        int currentLine = 1;
+        //Takes the number of lines as a parameter and sets that to the total lines for the overriding of read to end
+        public FibonacciTextReader(int numLines)
+        {
+            totalLines = numLines;
+        }
+
+        //Found Fibonacci algorithm from https://www.dotnetperls.com/fibonacci
+        public BigInteger FibonnaciNumberGenerator(int currentLine)
+        {
+            //These are big integers because normal integers can't hold the size of the later numbers in the sequence
+            BigInteger a = 0;
+            BigInteger b = 1;
+            BigInteger temp = 0;
+            //Needed for the fibonnaci sequence to start correctly
+            if (currentLine == 1)
+            {
+                return a;
+            }
+            //The fibonacci algorithm
+            for (int i = 1; i < currentLine; i++)
+            {
+                temp = a;
+                a = b;
+                b += temp;
+            }
+
+            return a;
+        }
+        
+        public override string ReadLine()
+        {
+            return FibonnaciNumberGenerator(currentLine).ToString();
+        }
+        //In combination with the overriden RealLine(), this correctly prints the fibonnaci sequence
+        public override string ReadToEnd()
+        {
+            string currentNumber = "";
+
+            while(currentLine <= totalLines)
+            {
+                currentNumber = currentNumber + (currentLine + ": " + ReadLine() + "\r\n");
+                currentLine++;
+            }
+
+            return currentNumber;
+        }
+    }
+}
---
+++ NotepadApp/NotepadApp HW/NotepadApp Homework/NotepadApp Homework/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NotepadApp_Homework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadText(TextReader sr)
+        {
+            //Clears the text box so another option can print to it
+            textBox1.Clear();
+            //Prints through the end of the file or string
+            textBox1.AppendText(sr.ReadToEnd());
+        }
+
+        private void loadFromFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Opens the load file dialog and filters so that only text files can be opened
+            OpenFileDialog loadFromFile = new OpenFileDialog();
+            loadFromFile.Filter = "Plain Text File (*.txt)|*.txt";
+            loadFromFile.ShowDialog();
+            //Prints the text from the file to the text box
+            using (StreamReader loadStream = new StreamReader(loadFromFile.FileName))
+            {
+                LoadText(loadStream);
+            }
+        }
+
+        private void load50FibonacciNumbersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Creates a new Fibonacci object to create the first 50 items of the fibonnaci sequence
+            FibonacciTextReader first50 = new FibonacciTextReader(50);
+            //Prints the contents of the object to the text box
+            LoadText(first50);
+        }
+
+        private void load100FibonacciNumbersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Creates a new Fibonacci object to create the first 100 items of the fibonnaci sequence
+            FibonacciTextReader first100 = new FibonacciTextReader(100);
+            //Prints the contents of the object to the text box
+            LoadText(first100);
+        }
+
+        private void saveToFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Opens the save file dialog and filters so that only text files can be saved to
+            SaveFileDialog saveToFile = new SaveFileDialog();
+            saveToFile.Filter = "Plain Text File (*.txt)|*.txt";
+            saveToFile.ShowDialog();
+            //Prints the text from the text box to the file
+            using (StreamWriter saveStream = new StreamWriter(saveToFile.FileName))
+            {
+                saveStream.Write(textBox1.Text);
+            }
+        }
+    }
+}
